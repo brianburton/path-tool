dbc0994e3bc8f99bd2f15bd4a962f95a957f5609
Brian Burton <brian@burton-computer.com>
2025-06-10 22:21:00 -0400

    Apply clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 
 fn main() {
-    let current = parse_path(&(env::var("PATH").unwrap_or(String::new())));
-    let mut args = env::args().skip(1).into_iter();
+    let current = parse_path(&(env::var("PATH").unwrap_or_default()));
     let mut inserted = false;
     let mut path = Vec::new();
-    while let Some(arg) = args.next() {
+    let args = env::args().skip(1);
+    for arg in args {
         if arg == "PATH" {
             if !inserted {
                 add_all(&mut path, &current);
@@ -31,17 +31,12 @@ fn remove(path: &mut Vec<String>, dir: &str) {
     }
 }
 
-fn add_first(path: &mut Vec<String>, dir: &str) {
-    remove(path, dir);
-    path.insert(0, dir.to_string());
-}
-
 fn add_last(path: &mut Vec<String>, dir: &str) {
     remove(path, dir);
     path.push(dir.to_string());
 }
 
-fn add_all(path: &mut Vec<String>, other: &Vec<String>) {
+fn add_all(path: &mut Vec<String>, other: &[String]) {
     for dir in other.iter() {
         add_unique(path, dir);
     }
@@ -56,9 +51,9 @@ fn add_unique(path: &mut Vec<String>, dir: &str) {
     path.push(dir.to_string());
 }
 
-fn parse_path(source: &String) -> Vec<String> {
+fn parse_path(source: &str) -> Vec<String> {
     let mut path: Vec<String> = Vec::new();
-    let mut remaining = source.as_str();
+    let mut remaining = source;
     while !remaining.is_empty() {
         match remaining.find(':') {
             Some(i) => {
@@ -74,6 +69,6 @@ fn parse_path(source: &String) -> Vec<String> {
     path
 }
 
-fn to_string(path: &Vec<String>) -> String {
+fn to_string(path: &[String]) -> String {
     path.join(":")
 }
